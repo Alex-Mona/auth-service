3af160c7abb2348c99b677d1a090f05d68caf94a
Alexander Monakov <101149383+Alex-Mona@users.noreply.github.com>
2024-12-15 00:57:24 +0300

    Delete refresh_tokens.sql

--- refresh_tokens.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE refresh_tokens (
-    id SERIAL PRIMARY KEY,
-    user_id UUID NOT NULL,
-    refresh_token_hash TEXT NOT NULL,
-    client_ip TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
-);
